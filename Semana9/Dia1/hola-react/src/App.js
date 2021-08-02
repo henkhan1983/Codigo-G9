ea0b692c725c6b1ff89d516a0fdfea657c1b4f38
henry de la cruz orellana <hendelacruz@gmail.com>
2021-08-01 23:20:50 -0500

    update 26072021_1

---
+++ Semana9/Dia1/hola-react/src/App.js
@@ -0,0 +1,14 @@
+
+export default function App() {
+  let titulo = " Mi App2"
+
+  return (
+    <div>
+      <h1>{titulo}</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, quibusdam?
+      </p>
+      <hr/>
+    </div>
+  )
+}
